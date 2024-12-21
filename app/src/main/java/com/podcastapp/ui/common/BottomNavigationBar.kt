ceda17cead7dd4e518d9dc7bb635b071a23e1e71
Gammarase <bogdankrivickij228@gmail.com>
2024-12-21 19:06:16 +0200

    bottom bar. ripple effect

--- app/src/main/java/com/podcastapp/ui/common/BottomNavigationBar.kt
+++ app/src/main/java/com/podcastapp/ui/common/BottomNavigationBar.kt
@@ -1,23 +1,30 @@
 package com.podcastapp.ui.common
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -29,7 +36,7 @@ fun BottomNavigationBar(
     navController: NavHostController,
     currentDestination: String,
     modifier: Modifier = Modifier,
-    iconSize: Dp = 32.dp
+    iconSize: Dp = 48.dp
 ) {
     val items = listOf(
         BottomNavItem("Home", Icons.Default.Home, MainFeature.mainScreenRoute),
@@ -45,8 +52,11 @@ fun BottomNavigationBar(
             items.forEach { item ->
                 Column(
                     modifier = Modifier
-                        .weight(1f)
-                        .clickable {
+                        .clip(RoundedCornerShape((iconSize * 2.5f)))
+                        .clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = rememberRipple(bounded = true, radius = (iconSize * 2.5f))
+                        ) {
                             if (currentDestination != item.route) {
                                 navController.navigate(item.route)
                             }
