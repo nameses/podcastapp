1c8e2281a87b67e5594850b2218fa8a9e604c642
Maxym <perebyinis.maksym@lll.kpi.ua>
2024-12-29 20:33:13 +0200

    dark theme block

--- app/src/main/java/com/podcastapp/ui/theme/Theme.kt
+++ app/src/main/java/com/podcastapp/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.core.common.theme.ColorWhite
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -35,14 +36,21 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun PodcastappTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val CustomLightColorScheme = lightColorScheme(
+        background = ColorWhite,
+        surface = ColorWhite,
+        primary = LightColorScheme.primary,
+        onPrimary = LightColorScheme.onPrimary
+    )
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicDarkColorScheme(context) else CustomLightColorScheme
         }
 
         darkTheme -> DarkColorScheme
